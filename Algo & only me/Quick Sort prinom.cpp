a4ebdd1b07a0fdad714d6f9360b58b4cb4bfe0a6
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-07-13 23:51:04 +0600

    Add files via upload

---
+++ Algo & only me/Quick Sort prinom.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    int pivot = arr[left];
+    int i = left + 1;
+    int j = right;
+
+    while (i <= j) {
+        while (i <= j && arr[i] <= pivot) {
+            i++;
+        }
+        while (i <= j && arr[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[left], arr[j]);
+
+    quickSort(arr, left, j - 1);
+    quickSort(arr, j + 1, right);
+}
+
+int main() {
+    int arr[] = {2, 3, 7, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
